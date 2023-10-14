ce1d19970856859305edc5ee0a5dbd3fc5bb835e
Danil Akhtarov <daxartio@gmail.com>
2023-10-14 22:38:25 +0300

    refactor: small changes

--- flager/main.py
+++ flager/main.py
@@ -1,5 +1,4 @@
 import logging
-from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional, Protocol, Set, Union
 
 import anyio
@@ -8,7 +7,6 @@ from .exceptions import FeatureFlagNotDefined, FeatureFlagNotFound
 
 __all__ = (
     "FeatureFlagsProxy",
-    "FeatureFlagsConfig",
     "FeatureFlagsUpdater",
 )
 
@@ -29,6 +27,20 @@ class Client(Protocol):
 
 
 class FeatureFlagsProxy:
+    """Proxy for feature flags.
+
+    A FeatureFlagsProxy class that acts as a proxy for feature flags.
+    It takes a dictionary of feature flags as input and a Client object
+    that is used to retrieve the feature flags from a server.
+    The __getitem__ method is overridden to return the value of a feature flag.
+    If the value is not in the cache, it is retrieved from the server using the get_flag
+    method of the Client object.
+    If the flag is not found on the server,
+    a FeatureFlagNotFound exception is raised,
+    unless the skip_not_found_error parameter is set to True.
+    If the flag is found, its value is returned.
+    """
+
     def __init__(
         self,
         feature_flags: FeatureFlags,
@@ -43,10 +55,10 @@ class FeatureFlagsProxy:
         self._skip_not_defined_error = skip_not_defined_error
         self._cache: Dict[str, bool] = {}
 
-    def __getitem__(self, name: str) -> Any:
-        original_val = self._feature_flags[name]
-        val = self._cache.get(name, original_val)
-        return self._get_from_server(name, val)
+    def __getitem__(self, key: str) -> Any:
+        original_val = self._feature_flags[key]
+        val = self._cache.get(key, original_val)
+        return self._get_from_server(key, val)
 
     async def _get_from_server(self, key: str, default: bool) -> bool:
         flag = await self._client.get_flag(key)
@@ -55,7 +67,7 @@ class FeatureFlagsProxy:
                 raise FeatureFlagNotFound(f"Feature flag {key} not found")
             logger.warning("Feature flag %s not found", key)
             return default
-        if flag.enabled is None:  # pragma: no cover
+        if flag.enabled is None:
             if not self._skip_not_defined_error:
                 raise FeatureFlagNotDefined(f"Feature flag {key} is None")
             logger.warning("Feature flag %s is None", key)
@@ -72,15 +84,19 @@ class FeatureFlagsProxy:
         return flag.enabled
 
 
-@dataclass
-class FeatureFlagsConfig:
-    server_url: str
-    feature_flags: List[FeatureFlags] = field(default_factory=list)
-    interval: int = 15
-    skip_errors: bool = True
-
-
 class FeatureFlagsUpdater:
+    """Feature flags updater.
+
+    A FeatureFlagsUpdater class that is responsible
+    for updating the feature flags periodically.
+    It takes a list of FeatureFlags objects as input,
+    along with a Client object that is used to retrieve the feature flags from a server.
+    The interval parameter specifies the time interval (in seconds) between updates.
+    The skip_client_errors, skip_not_defined_error, and skip_not_found_error parameters
+    control whether errors are skipped
+    when retrieving the feature flags from the server.
+    """
+
     def __init__(
         self,
         feature_flags: List[FeatureFlags],
@@ -105,20 +121,19 @@ class FeatureFlagsUpdater:
     def _get_keys(self) -> List[str]:
         keys: Set[str] = set()
         for feature_flags in self._feature_flags:
-            for field_name in feature_flags.keys():
-                keys.add(field_name)
+            for key in feature_flags.keys():
+                keys.add(key)
         return list(keys)
 
     def _update_feature_flags(self, flags_by_keys: Dict[str, bool]) -> None:
         for feature_flags in self._feature_flags:
-            for field_name in feature_flags.keys():
-                key = field_name
+            for key in feature_flags.keys():
                 if key not in flags_by_keys:
                     continue
-                current_val = feature_flags[field_name]
+                current_val = feature_flags[key]
                 new_val = flags_by_keys[key]
                 if current_val != new_val:
-                    feature_flags[field_name] = new_val
+                    feature_flags[key] = new_val
                     logger.info(
                         "Feature flag %s was changed from %s to %s",
                         key,
