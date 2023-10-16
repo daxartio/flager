93205da195ed67de3b7479af32736985a76a71c2
Danil Akhtarov <daxartio@gmail.com>
2023-10-16 20:45:58 +0300

    chore(release): version 0.1.1 → 0.1.2

--- flager/__init__.py
+++ flager/__init__.py
@@ -1,7 +1,7 @@
 from flager.exceptions import BaseError, FeatureFlagNotDefined, FeatureFlagNotFound
 from flager.main import FeatureFlagsProxy, FeatureFlagsUpdater
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = (
     "BaseError",
     "FeatureFlagNotDefined",
